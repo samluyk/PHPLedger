b6d33622ae5737a23ec0a32134f18596cd7f0e81
Samuel Luyk <sluyk@reverus.com>
2019-12-06 15:14:10 -0500

    HTML Best Practices

--- deletelatest.php
+++ deletelatest.php
@@ -6,7 +6,10 @@ date_default_timezone_set("America/New_York");
 
 <html>
     <head>
+    <meta charset='UTF-8'/>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <meta name='viewport'
+      content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
     </head>
     <title>Delete Latest</title>
 
--- index.php
+++ index.php
@@ -7,6 +7,9 @@ Project: PHPLedger
 -->
 <html>
 <head>
+<meta charset='UTF-8'/>
+<meta name='viewport'
+      content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <title>Financial Logger</title>
