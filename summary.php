85185b1165a389be38d46e683df4205fe19a22c3
Sam <samluyk@protonmail.com>
2019-12-07 13:29:16 -0500

    Begin summary page (to copy spreadsheet look)

---
+++ summary.php
@@ -0,0 +1,85 @@
+<!doctype html>
+
+<?php
+date_default_timezone_set("America/New_York");
+?>
+
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <title>Financial Summary</title>
+
+    <h1>Database GUI</h1>
+    <hr class="new1">
+    <br>
+    <center>Financial Overview</center>
+
+    <table cellspacing='0'> <!-- cellspacing='0' is important, must stay -->
+
+	<!-- Table Header -->
+	<thead>
+		<tr>
+			<th>Task Details</th>
+			<th>Progress</th>
+			<th>Vital Task</th>
+		</tr>
+	</thead>
+	<!-- Table Header -->
+
+	<!-- Table Body -->
+	<tbody>
+
+		<tr>
+			<td>Create pretty table design</td>
+			<td>100%</td>
+			<td>Yes</td>
+		</tr><!-- Table Row -->
+
+		<tr class="even">
+			<td>Take the dog for a walk</td>
+			<td>100%</td>
+			<td>Yes</td>
+		</tr><!-- Darker Table Row -->
+
+		<tr>
+			<td>Waste half the day on Twitter</td>
+			<td>20%</td>
+			<td>No</td>
+		</tr>
+
+		<tr class="even">
+			<td>Feel inferior after viewing Dribble</td>
+			<td>80%</td>
+			<td>No</td>
+		</tr>
+
+		<tr>
+			<td>Wince at "to do" list</td>
+			<td>100%</td>
+			<td>Yes</td>
+		</tr>
+
+		<tr class="even">
+			<td>Vow to complete personal project</td>
+			<td>23%</td>
+			<td>yes</td>
+		</tr>
+
+		<tr>
+			<td>Procrastinate</td>
+			<td>80%</td>
+			<td>No</td>
+		</tr>
+
+		<tr class="even">
+			<td><a href="#yep-iit-doesnt-exist">Hyperlink 	Example</a></td>
+			<td>80%</td>
+			<td><a href="#inexistent-id">Another</a></td>
+		</tr>
+
+	</tbody>
+	<!-- Table Body -->
+
+</table>
+</html>
