b5370fe1e3a319f6ca848fe3150f6036314dd44a
Sam <samluyk@protonmail.com>
2019-12-07 12:14:22 -0500

    graphs, maybe

--- query.php
+++ query.php
@@ -2,13 +2,106 @@
 
 <?php
 date_default_timezone_set("America/New_York");
+
+/* Database connection settings */
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'test_database';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+$data1 = '';
+$data2 = '';
+
+//query to get data from the table
+$sql = "SELECT * FROM `expenses` ";
+$result = mysqli_query($mysqli, $sql);
+
+//loop through the returned data
+while ($row = mysqli_fetch_array($result)) {
+
+    $data1 = $data1 . '"'. $row['expense_id'].'",';
+    $data2 = $data2 . '"'. $row['the_expense'] .'",';
+}
+
+$data1 = trim($data1,",");
+$data2 = trim($data2,",");
+
 ?>
 
+<!DOCTYPE html>
 <html>
+	<head>
     <head>
     <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <title>Run a query</title>
+        <title>Run a query</title>
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+
+
+		<style type="text/css">			
+			body{
+				font-family: Arial;
+			    margin: 80px 100px 10px 100px;
+			    padding: 0;
+			    color: white;
+			    text-align: center;
+			    background: #555652;
+			}
+
+			.container {
+				color: #E8E9EB;
+				background: #222;
+				border: #555652 1px solid;
+				padding: 10px;
+			}
+		</style>
+
+	</head>
+
+	<body>	   
+	    <div class="container">	
+	    <h1>USE CHART.JS WITH MYSQL DATASETS</h1>       
+			<canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
+
+			<script>
+				var ctx = document.getElementById("chart").getContext('2d');
+    			var myChart = new Chart(ctx, {
+        		type: 'line',
+		        data: {
+		            labels: [1,2,3,4,5,6,7,8,9],
+		            datasets: 
+		            [{
+		                label: 'Data 1',
+		                data: [<?php echo $data1; ?>],
+		                backgroundColor: 'transparent',
+		                borderColor:'rgba(255,99,132)',
+		                borderWidth: 3
+		            },
+
+		            {
+		            	label: 'Data 2',
+		                data: [<?php echo $data2; ?>],
+		                backgroundColor: 'transparent',
+		                borderColor:'rgba(0,255,255)',
+		                borderWidth: 3	
+		            }]
+		        },
+		     
+		        options: {
+		            scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},
+		            tooltips:{mode: 'index'},
+		            legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}}
+		        }
+		    });
+			</script>
+	    </div>
+	    
+	</body>
+</html>
+<html>
+
 
     <h1>Database GUI</h1>
     <hr class="new1">
