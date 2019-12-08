e1a9ecc544a6aa1bc97b4f5d7209ddb975c9d25a
Sam <samluyk@protonmail.com>
2019-12-08 12:27:05 -0500

    Initial commit for edit category page

---
+++ editcategory.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<!--
+/////////////////////////////////////////////////////////////////////////
+This page allows us to edit the categories
+/////////////////////////////////////////////////////////////////////////
+-->
+<html>
+<head>
+<meta charset='UTF-8'/>
+<meta name='viewport'
+      content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<title>Financial Logger</title>
+
+<body>
+<center>
+<h1>Edit Categories</h1>
+<hr class="new1">
+beep boop
+<?php
+/*
+$a=array("red","green");
+array_push($a,"blue","yellow");
+print_r($a);
+*/
+?> 
