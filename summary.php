9f63af0d9f2c01cd2c355b6fa478a1142f40c9d1
Samuel Luyk <sluyk@reverus.com>
2020-01-07 11:50:23 -0500

    Updated summarization page

--- summary.php
+++ summary.php
@@ -22,10 +22,8 @@ if($connection === false){
     </head>
     <title>Financial Summary</title>
 
-    <h1>Database GUI</h1>
+    <h1>Financial Overview</h1>
     <hr class="new1">
-    <br>
-    <center>Financial Overview</center>
 	<br>
 <center>
     <table cellspacing='0'> <!-- cellspacing='0' is important, must stay -->
@@ -57,137 +55,181 @@ if($connection === false){
 	<tbody>
 
 		<tr>
-			<td>Create pretty table design</td>
+			<td>Car</td>
 			<td>100%</td>
 			<td>Yes</td>
 		</tr><!-- Table Row -->
 
 		<tr class="even">
-			<td>Take the dog for a walk</td>
+			<td>Education</td>
 			<td>100%</td>
 			<td>Yes</td>
 		</tr><!-- Darker Table Row -->
 
 		<tr>
-			<td>Waste half the day on Twitter</td>
+			<td>Entertainment</td>
 			<td>20%</td>
 			<td>No</td>
 		</tr>
 
 		<tr class="even">
-			<td>Feel inferior after viewing Dribble</td>
+			<td>Food</td>
 			<td>80%</td>
 			<td>No</td>
 		</tr>
 
 		<tr>
-			<td>Wince at "to do" list</td>
+			<td>Giving</td>
 			<td>100%</td>
 			<td>Yes</td>
 		</tr>
 
 		<tr class="even">
-			<td>Vow to complete personal project</td>
+			<td>Hobbies</td>
 			<td>23%</td>
 			<td>yes</td>
 		</tr>
 
 		<tr>
-			<td>Procrastinate</td>
+			<td>Home</td>
 			<td>80%</td>
 			<td>No</td>
 		</tr>
 
         <tr class="even">
-			<td>Waste half the day on Twitter</td>
+			<td>Medical</td>
 			<td>20%</td>
 			<td>No</td>
         </tr>
         <tr>
-			<td>Waste half the day on Twitter</td>
+			<td>Miscellaneous</td>
 			<td>20%</td>
 			<td>No</td>
         </tr>
         <tr class="even">
-			<td>Waste half the day on Twitter</td>
+			<td>Personal</td>
 			<td>20%</td>
 			<td>No</td>
         </tr>
         <tr>
-			<td>Waste half the day on Twitter</td>
+			<td>Savings</td>
 			<td>20%</td>
 			<td>No</td>
         </tr>
         <tr class="even">
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr>
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr class="even">
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr>
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr class="even">
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr>
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr class="even">
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr>
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr class="even">
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr>
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr class="even">
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr>
-			<td>Waste half the day on Twitter</td>
-			<td>20%</td>
-			<td>No</td>
-        </tr>
-        <tr class="even">
-			<td>Waste half the day on Twitter</td>
+			<td>Utilities</td>
 			<td>20%</td>
 			<td>No</td>
         </tr>
         
-
 	</tbody>
 	<!-- End Table Body -->
-
 </table>
+<hr class="new1">
+</center>
+
+<!-- 2020 -->
+
+<center>
+    <table cellspacing='0'> <!-- cellspacing='0' is important, must stay -->
+
+	<!-- Table Header -->
+	<thead>
+		<tr>
+			<th>2020 Budget</th>
+			<th>January</th>
+            <th>February</th>
+            <th>March</th>
+            <th>April</th>
+            <th>May</th>
+            <th>June</th>
+            <th>July</th>
+            <th>August</th>
+            <th>September</th>
+            <th>October</th>
+            <th>November</th>
+            <th>December</th>
+            <th>Year Total</th>
+            <th>Monthly Averge</th>
+            
+		</tr>
+	</thead>
+	<!-- Table Header -->
+
+	<!-- Table Body -->
+	<tbody>
+
+		<tr>
+			<td>Car</td>
+			<td>100%</td>
+			<td>Yes</td>
+		</tr><!-- Table Row -->
+
+		<tr class="even">
+			<td>Education</td>
+			<td>100%</td>
+			<td>Yes</td>
+		</tr><!-- Darker Table Row -->
+
+		<tr>
+			<td>Entertainment</td>
+			<td>20%</td>
+			<td>No</td>
+		</tr>
+
+		<tr class="even">
+			<td>Food</td>
+			<td>80%</td>
+			<td>No</td>
+		</tr>
+
+		<tr>
+			<td>Giving</td>
+			<td>100%</td>
+			<td>Yes</td>
+		</tr>
+
+		<tr class="even">
+			<td>Hobbies</td>
+			<td>23%</td>
+			<td>yes</td>
+		</tr>
+
+		<tr>
+			<td>Home</td>
+			<td>80%</td>
+			<td>No</td>
+		</tr>
+
+        <tr class="even">
+			<td>Medical</td>
+			<td>20%</td>
+			<td>No</td>
+        </tr>
+        <tr>
+			<td>Miscellaneous</td>
+			<td>20%</td>
+			<td>No</td>
+        </tr>
+        <tr class="even">
+			<td>Personal</td>
+			<td>20%</td>
+			<td>No</td>
+        </tr>
+        <tr>
+			<td>Savings</td>
+			<td>20%</td>
+			<td>No</td>
+        </tr>
+        <tr class="even">
+			<td>Utilities</td>
+			<td>20%</td>
+			<td>No</td>
+        </tr>
+        
+	</tbody>
+	<!-- End Table Body -->
+</table>
+<hr class="new1">
 </center>
 </html>
